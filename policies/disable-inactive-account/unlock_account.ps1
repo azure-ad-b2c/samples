848c718272a0d38b0b873d339bd2864330b9a171
Jas Suri <jasuri@microsoft.com>
2019-03-20 21:59:28 +0000

    update tenant refs

--- policies/disable-inactive-account/unlock_account.ps1
+++ policies/disable-inactive-account/unlock_account.ps1
@@ -4,20 +4,20 @@ $oauth = $null
 $ClientID      = ""                # Should be a ~35 character string insert your info here
 $ClientSecret  = ""         
 $loginURL      = "https://login.microsoftonline.com"
-$tenantdomain  = $tenant        # For example, contoso.onmicrosoft.com
 $resource = "https://graph.windows.net"
 
 # Get an Oauth 2 access token based on client id, secret and tenant domain
 $body       = @{grant_type="client_credentials";client_id=$ClientID;client_secret=$ClientSecret;resource=$resource}
-$oauth      = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+$oauth      = Invoke-RestMethod -Method Post -Uri $loginURL/$tenant/oauth2/token?api-version=1.0 -Body $body
 
 #find user
+Connect-AzureAD
 get-azureaduser -all $true | Where-Object {$_.signinnames.value -eq "somebody@microsoft.com"} | fl accountenabled, objectid
 get-azureaduser -all $true | Where-Object {$_.signinnames.value -eq "somebody@microsoft.com"} | Select-Object -ExpandProperty extensionproperty
 
 #update user
 $objectId = "insert-objectid"
-$url = "https://graph.windows.net/b2cprod.onmicrosoft.com/users/$objectId`?api-version=1.6"
+$url = "https://graph.windows.net/$tenant/users/$objectId`?api-version=1.6"
 
 $currentdate = (Get-Date).ToUniversalTime()
 $body = @"
