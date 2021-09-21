d6e6ff968bb59904c8e33801fecd2206dccb0a9b
David Hoerster <david@agileways.com>
2021-09-21 10:48:09 -0400

    move existing code to netcore22 folder; add net50 code to own folder

---
+++ policies/sign-in-with-magic-link/source-code/netcore2.2/AADB2C.MagicLink.WebApi/Models/OidcModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AADB2C.MagicLink.WebApi.Models
+{
+    public class OidcModel
+    {
+        [JsonProperty("issuer")]
+        public string Issuer { get; set; }
+
+        [JsonProperty("jwks_uri")]
+        public string JwksUri { get; set; }
+
+        [JsonProperty("id_token_signing_alg_values_supported")]
+        public ICollection<string> IdTokenSigningAlgValuesSupported { get; set; }
+    }
+}
