7db33455bf2fde8d15d5c6f058e429d83a7db491
JasSuri <jasuri@microsoft.com>
2023-09-11 10:31:53 +0100

    upgrade-ready-arch-init

---
+++ policies/migrate-to-entra-external-id-for-customers/readme.md
@@ -0,0 +1,50 @@
+# Upgrade ready architecture: AAD B2C to Entra External Id for Customers
+
+## Summary
+
+This sample will use AAD B2C as the journey orchestrator, whilst creating and authenticating users in the Entra External Id tenant. This makes it easier in the future to move apps to Entra External Id without disruption to your users. This sample performs sign up/in with MFA using Azure AD B2C, whilst maintaining user profiles in the Entra External Id tenant.
+
+## How it works
+
+Users are sent to the AAD B2C authentiaction endpoint. An Azure function orchestrates all Read/Write operations to the Entra External Id directory.
+
+![High level design](media/high-level-design.png)
+
+![Sign up with MFA](media/signup.png)
+
+![Sign in with MFA](media/signin.png)
+
+## How to set it up
+
+### Create application registrations
+1. Create an Application registration in the Entra External Id tenant, named **RopcFromB2C**. Choose Native App. Copy the AppId/ClientId
+1. In the Authentication menu, enable **Allow public client**
+
+1. Create an Application registration in the Entra External Id tenant, named **GraphCallsFromB2CTenant**. Choose Web App. Copy the AppId/ClientId
+1. Under API permissions, add MS Graph API **Application** permissions: `User.ReadWrite.All` and `UserAuthenticationMethod.ReadWrite.All`
+1. Under **Certificates & secrets**, generate a new secret. Copy this secret to be placed in the Azure function code
+
+### Modifications in Azure Function
+1. Change all instances of `your-tenant-id-guid` to your tenant guid or name eg `contoso.onmicrosoft.com`
+1. Change all instances of `your-clientId-RopcFromB2C` to the AppId of the **RopcFromB2C** App registration
+1. Change all instances of `your-client-id-to-call-graph` to the AppId of the **GraphCallsFromB2CTenant** App registration
+1. Change all instances of `your-client-secret` to the secret generated on the **GraphCallsFromB2CTenant** App registration
+
+### Host the Azure function
+1. In any production scenario, enable authentication in the Azure function. Eg, OAuth.
+
+### Modifications in policy file
+1. Change all instances of `your-tenant` to your tenant name, eg: `contoso.onmicrosoft.com`
+1. Change all instances of `your-api-endpoint` to your Azure function endpoint.
+1. In any production scenario, enable authentication in the REST API technical profiles.
+
+## Testing
+Scenarios to test:
+1. Sign up via this AAD B2C custom policy with an account that does not already exist
+1. Sign in via this AAD B2C custom policy with an account that does exist
+1. Sign up via this AAD B2C custom policy with an account that does already exist
+1. Sign in via this AAD B2C custom policy with an account that does not exist
+1. Assign the user to a group/role in Entra External Id tenant, then perform a sign in
+1. Create a User Flow in Entra External Id tenant, and perform a Sign In via Entra External Id tenant with an account that was created via this AAD B2C custom policy
+1. Sign in via this AAD B2C custom policy with an account that was created using the User Flow from Entra External Id tenant
+1. View the User Profile after Sign Up in the Entra External Id tenant. Ensure the `Authentication Methods` menu is correctly populated with the users phone number visible, after sign up via this AAD B2C custom policy
---
+++ policies/migrate-to-entra-external-id-for-customers/source-code/ciamHelper.cs
@@ -0,0 +1,202 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Azure.Identity;
+using Microsoft.Graph.Beta;
+using Microsoft.Graph.Beta.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+using System.Net.Http;
+
+namespace readUser
+{
+    public static class ciamHelper
+    {
+        [FunctionName("ciamHelper")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string objectId = data.objectId;
+            string email = data.email;
+            string password = data.password;
+            string method = data.method;
+            string phoneNumber = data.phoneNumber;
+            string displayName = data.displayName;
+            string givenName = data.givenName;
+            string surName = data.surName;
+
+            var tenantId = "your-tenant-id-guid";
+
+
+            if (method == "auth")
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    // Build the request URL
+                    var requestUrl = "https://ciamprod.ciamlogin.com/c9d7a627-dfab-45ba-92ee-54b19379dc3d/oauth2/token";
+                    string auth_resource = "https://graph.microsoft.com"; // Replace with your specific resource URL
+                    string auth_clientId = "your-clientId-RopcFromB2C";
+
+                    // Prepare the request body
+                    var auth_requestBody = $"resource={auth_resource}&client_id={auth_clientId}&grant_type=password&username={email}&password={password}&nca=1";
+
+                    // Convert the request body to a byte array
+                    var content = new StringContent(auth_requestBody, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+                    // Send the POST request to Azure AD
+                    using (var response = await httpClient.PostAsync(requestUrl, content))
+                    {
+                        // Check if the request was successful
+                        if (response.IsSuccessStatusCode)
+                        {
+                            // Read the response content
+                            var responseContent = await response.Content.ReadAsStringAsync();
+                            var jsonObj = JsonConvert.DeserializeObject(responseContent);
+                            return new OkObjectResult(jsonObj);
+                        }
+                        else
+                        {
+                            // Handle error cases here
+                            // For example, log the error or throw an exception
+                            return new ConflictObjectResult(new B2CResponseModel($"Invalid username or password.", HttpStatusCode.Conflict));
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+                var scopes = new[] { "https://graph.microsoft.com/.default" };
+                // Values from app registration  
+                var clientId = "your-client-id-to-call-graph";
+                var clientSecret = "your-client-secret";
+
+                // using Azure.Identity;  
+                var options = new TokenCredentialOptions
+                {
+                    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+                };
+
+                // https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential  
+                var clientSecretCredential = new ClientSecretCredential(
+                    tenantId, clientId, clientSecret, options);
+
+                // get accessToken          
+                var accessToken = await clientSecretCredential.GetTokenAsync(new Azure.Core.TokenRequestContext(scopes) { });
+
+                Console.WriteLine(accessToken.Token);
+
+                var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+                if (objectId != null && method == "read")
+                {
+                    var user = await graphClient.Users[objectId].GetAsync();
+                    log.LogInformation(user.ToString());
+                    return new OkObjectResult(user);
+                }
+
+                if (email != null && method == "read")
+                {
+                    var user = await graphClient.Users.GetAsync((requestConfiguration) =>
+                    {
+                        requestConfiguration.QueryParameters.Filter = string.Format("identities/any(x:x/issuerAssignedId eq '{0}' and x/issuer eq 'ciamprod.onmicrosoft.com')  ", email);
+                    });
+                    return new OkObjectResult(user);
+                }
+
+                if (method == "createUser")
+                {
+                    var userRequestBody = new User
+                    {
+                        DisplayName = displayName,
+                        Identities = new List<ObjectIdentity>
+                    {
+                        new ObjectIdentity
+                        {
+                            SignInType = "emailAddress",
+                            Issuer = "ciamprod.onmicrosoft.com",
+                            IssuerAssignedId = email,
+                        }
+                    },
+                        PasswordProfile = new PasswordProfile
+                        {
+                            Password = password,
+                            ForceChangePasswordNextSignIn = false,
+                        },
+                        PasswordPolicies = "DisablePasswordExpiration",
+                    };
+                    try
+                    {
+                        var result = await graphClient.Users.PostAsync(userRequestBody);
+                        return new OkObjectResult(result);
+                    }
+                    catch (Exception ex)
+                    {
+                        return new ConflictObjectResult(new B2CResponseModel($"This account already exists.", HttpStatusCode.Conflict));
+                    }
+                }
+
+                if (method == "getPhone")
+                {
+                    try
+                    {
+                        var result = await graphClient.Users[objectId].Authentication.PhoneMethods.GetAsync();
+                        return new OkObjectResult(result.Value[0]);
+                    }
+
+                    catch (Exception exception)
+                    {
+                        var jsonObject = new JObject();
+                        jsonObject.Add("phoneNumber", "null");
+                        return new OkObjectResult(jsonObject);
+                    }
+
+                }
+
+                if (method == "setPhone")
+                {
+
+                    var mfaRequestBody = new PhoneAuthenticationMethod
+                    {
+                        PhoneNumber = phoneNumber,
+                        PhoneType = AuthenticationPhoneType.Mobile,
+                    };
+                    var enrolResult = await graphClient.Users[objectId].Authentication.PhoneMethods.PostAsync(mfaRequestBody);
+                    return new OkObjectResult(enrolResult);
+                }
+            }
+
+            return new OkObjectResult(null);
+        }
+
+    }
+
+
+    public class B2CResponseModel
+    {
+        public string version { get; set; }
+        public int status { get; set; }
+        public string userMessage { get; set; }
+
+        public B2CResponseModel(string message, HttpStatusCode status)
+        {
+            this.userMessage = message;
+            this.status = (int)status;
+            this.version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
